98302fdbed23e2762a62a7e2ec27e7a9edfe7515
Ujjwal <ujjwaljindal835@gmail.com>
2024-03-29 17:37:53 +0530

    created version 1 of todo project only frontend

---
+++ projects/1-todo-app-version-1/src/components/AppName.jsx
@@ -0,0 +1,5 @@
+export default function AppName() {
+    return (
+        <h1>TODO React App</h1>
+    )
+}
---
+++ projects/1-todo-app-version-1/src/components/TodoItem1.jsx
@@ -0,0 +1,13 @@
+export default function TodoItem1() {
+    let todoName = 'Buy Milk';
+    let todoDate = '04/10/2023';
+    return (
+        <div className="container">
+            <div className="row kg-row">
+                <div className="col-6">{todoName}</div>
+                <div className="col-4">{todoDate}</div>
+                <div className="col-2"><button type="button" className="btn btn-danger kg-button">Delete</button></div>
+            </div>
+        </div>
+    )
+}
