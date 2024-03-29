b3ac468cc21d46c8f79fb003c415e23dfabfbd2f
Ujjwal <ujjwaljindal835@gmail.com>
2024-03-29 14:30:05 +0530

    learned compoent making and bootstrapping

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Button from "./Button";
+import Hello from "./Hello";
+import Random from "./Random";
+function App() {
+  return <div>
+    <h1>Hello World This is the best react course
+    </h1>
+    <Button />
+    <Hello />
+    <Random />
+    <Random />
+    <Random />
+  </div>
+
+}
+export default App;
---
+++ first-react-app/src/Hello.jsx
@@ -0,0 +1,11 @@
+export default function Hello() {
+    let myName = 'Ujjwal';
+    let number =456;
+        let fullName = () => {
+            return 'Ujjwal Jindal'
+        }
+    return<>
+     <h3>Hello this is future speaking. </h3>
+    <p>Message No. : {number} My name is {fullName()}</p>
+    </>
+}
