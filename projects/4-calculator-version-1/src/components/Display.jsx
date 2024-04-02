dafeeffa84756ea8207ceab47b14da5e3b7484e5
Ujjwal <ujjwaljindal835@gmail.com>
2024-04-02 16:23:44 +0530

    created a calculator app using react without functionality , used map methods and components

---
+++ projects/4-calculator-version-1/src/components/Display.jsx
@@ -0,0 +1,8 @@
+import styles from './Display.module.css'
+export default function Display() {
+    return (
+        <div>
+            <input className={styles.display} type="text" />
+        </div>
+    )
+}
